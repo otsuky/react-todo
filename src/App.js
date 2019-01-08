dd4b342b019aeef8b6ead08a532d57ccb33bec64
大月佑佳子 <yukako.otsuki@nijibox.co.jp>
2019-01-08 19:11:43 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './TodoList';
+import './App.scss';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: "やること1",
+          desc: "やることやることやること",
+          done: false
+        },
+        {
+          id: 2,
+          title: "やること2",
+          desc: "やることやることやることやること",
+          done: false
+        },
+      ]
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <h1>todoリスト</h1>
+        <TodoList
+          todos = {this.state.todos}
+          />
       </div>
     );
   }
